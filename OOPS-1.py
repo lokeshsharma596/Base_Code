f3d5f7be75b86ae9565ba18e11f2aa85ffbb85af
Lokesh Sharma <41072916+lokeshsharma596@users.noreply.github.com>
2019-09-23 16:49:01 +0530

    Update OOPS-1.py

--- OOPS-1.py
+++ OOPS-1.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Class and Object:
-
-# In[78]:
-
-
 class person:
     def __init__(self,name,age,company):
         self.name=name
@@ -17,39 +9,21 @@ class person:
         print(self.email)
 
 
-# In[79]:
-
-
 e1=person('lokesh',34,'hcl')
 e2=person('xyz',54,'poc')
-
-
-# In[80]:
-
 
 print(e1.name)
 print(e2.age)
 
 
-# In[81]:
-
-
 e1.email()
-
-
-# In[82]:
 
 
 del e2.name
 
 
-# In[83]:
-
-
 e2.name="hello"
 
-
-# In[84]:
 
 
 print(e2.name)
@@ -61,29 +35,16 @@ print(e2.name)
 del e1,e2
 
 
-# # Inheritance:
-
-# In[93]:
-
-
 class student(person):
     pass
-
-
-# In[94]:
 
 
 s1=student('lok',67,'goog')
 
 
-# In[95]:
-
-
 print(s1.name ,s1.age,s1.company)
 s1.email()
 
-
-# In[101]:
 
 
 ##adding __init__ to child
@@ -92,15 +53,10 @@ s1.email()
 #To keep the inheritance of the parent's __init__() function, add a call to the parent's __init__() function.
 
 
-# In[104]:
-
 
 class student(person):
     def __init__(self,name,age,company):
         pass
-
-
-# In[106]:
 
 
 class student(person):
